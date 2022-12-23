55f6039fd8bb07961b72b6c4bbce5795a1d1f378
Prasannadjango <Prasannakumarprasanna1199@gmail.com>
2022-12-23 12:49:32 +0530

    changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,8 +7,8 @@ import { Sidebar, Menu, MenuItem, SubMenu, useProSidebar } from "react-pro-sideb
 function Sidemenu() {
    return (
       <>
-         <Sidebar>
-            <Menu>
+         <Sidebar >
+            <Menu className="bg-white">
                <MenuItem icon={<FaIcons.FaHome className="fs-5" />} routerLink={<Link to="/home" />} > Dashboard</MenuItem>
                <MenuItem icon={<FaIcons.FaChalkboardTeacher className="fs-5" />} routerLink={<Link to="/Manageteachers" />} >Manage Teachers</MenuItem>
                <MenuItem icon={<FaIcons.FaUserGraduate className="fs-5" />} routerLink={<Link to="/Managestudents" />} >Manage Students</MenuItem>
